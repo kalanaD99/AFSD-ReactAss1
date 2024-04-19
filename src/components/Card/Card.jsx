c80deca57b06314c45fe77926657c83bc939e688
kalanad99 <kalanadananjaya855@gmail.com>
2024-04-19 17:32:18 +0530

    initial commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Card.css'
+
+export  function Card() {
+  return (
+    <div className='container'>
+        <h2 className='name'>
+            Kalana Dananjaya 
+        </h2>
+        <p className='para'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Tenetur, hic nemo dolorem quidem enim officia 
+            ipsam laudantium nesciunt nam ut!
+        </p>
+        <div className='address'>
+            Address : Beruwala 
+        </div>
+        <div className='age'>
+            age : 24
+        </div>
+
+    </div>
+  )
+}
+
+
+export  function Card2() {
+    return (
+      <div className='container'>
+          <h2 className='name1'>
+              Kalana Dananjaya 
+          </h2>
+          <p className='para1'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Tenetur, hic nemo dolorem quidem enim officia 
+              ipsam laudantium nesciunt nam ut!
+          </p>
+          <div className='address1'>
+              Address : Beruwala 
+          </div>
+          <div className='age1'>
+              age : 24
+          </div>
+  
+      </div>
+    )
+  }
+
+  export  function Card3() {
+    return (
+      <div className='container1'>
+          <h2 className='name2'>
+              Kalana Dananjaya 
+          </h2>
+          <p className='para2'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Tenetur, hic nemo dolorem quidem enim officia 
+              ipsam laudantium nesciunt nam ut!
+          </p>
+          <div className='address2'>
+              Address : Beruwala 
+          </div>
+          <div className='age2'>
+              age : 24
+          </div>
+  
+      </div>
+    )
+  }
